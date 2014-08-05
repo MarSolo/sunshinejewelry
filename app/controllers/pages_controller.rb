9d9aa07c9b373513827897acc55cebf7f092e8fc
Mario <mariosolo@gmail.com>
2014-08-04 20:52:18 -0400

    bundle install, tim eofr heroku push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,45 +1,4 @@
 class PagesController < ApplicationController
   def home
   end
-  
-  def about
-      def services
-      end
-
-      def goldsmith
-      end
-
-      def mission
-      end
-  end
-
-  def products
-      def jewelry
-      end
-
-      def necklaces
-      end
-
-      def earrings
-      end
-
-      def rings
-      end
-
-      def blackpearls
-      end
-
-      def children
-      end
-
-      def bridal
-      end
-
-      def christmas
-      end
-  end
-
-  def contact
-  end
-
 end
