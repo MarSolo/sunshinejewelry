425d19d949bf05ba8e47de0d3b1714a7cb28cfe6
Mario <mariosolo@gmail.com>
2014-08-04 20:45:59 -0400

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,45 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def about
+      def services
+      end
+
+      def goldsmith
+      end
+
+      def mission
+      end
+  end
+
+  def products
+      def jewelry
+      end
+
+      def necklaces
+      end
+
+      def earrings
+      end
+
+      def rings
+      end
+
+      def blackpearls
+      end
+
+      def children
+      end
+
+      def bridal
+      end
+
+      def christmas
+      end
+  end
+
+  def contact
+  end
+
+end
---
+++ config/initializers/devise.rb
@@ -0,0 +1,9 @@
+Devise.setup do |config|
+# The secret key used by Devise. Devise uses this key to generate
+# random tokens. Changing this key will render invalid all existing
+# confirmation, reset password and unlock tokens in the database.
+  config.secret_key = "2ebc20181741264e728b0804620f4bf2fc7fe1b05f5dff39885422a58a573f5508f60745ce9790f1493835eae334242bd9712e8fe03a16e6b24f67c914b468af"
+# -
+# -
+# etc
+end
