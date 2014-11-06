6eaa2302e0d249e627c30f2decfd9aaa6126ecee
Mario <mariosolo@gmail.com>
2014-11-05 19:01:20 -0500

    default image

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ActiveRecord::Base
     validates :text, presence: true
 
     has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100>" }
+    has_attached_file :image, :default_url => "/images/sunshine_logo.jpg"
     validates :image, presence: false
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
